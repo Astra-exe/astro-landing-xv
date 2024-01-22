978585274e0b3dc46647a57f7e411df3d34ddf51
francisco-solis99 <franciscosoliscss@gmail.com>
2024-01-22 14:02:16 -0600

    style: add the fonts and the config in the styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				poppins: ['Poppins', 'system-ui', 'sans-serif'],
+				parisienne: ['Parisienne', 'cursive']
+			},
+			colors: {
+				'background-body': '#F6F4EC',
+				'primary': '#FFA5A5',
+				'secundary': '#FFD6C0',
+				'text-dark': '#262730'
+			},
+			backgroundImage: {
+
+      }
+		},
 	},
 	plugins: [],
 }
